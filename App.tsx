01c7d392825314658b993bb567d3fd6bb98e611f
avnishranwa7 <avnishranwa7@gmail.com>
2024-06-17 12:16:22 +0530

    added splash screen

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-import { useState } from "react";
 import { SafeAreaView, StatusBar, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { Provider } from "react-redux";
@@ -8,19 +7,10 @@ import { Provider } from "react-redux";
 // local imports
 import { store } from "./store/index";
 import Navigator from "./Navigator";
-import LoadingScreen from "./SplashScreen";
 import { Color } from "./constants/Color";
 
 export default function App() {
-  const [loading, setLoading] = useState(true);
-
-  setTimeout(() => {
-    setLoading(false);
-  }, 1000);
-
-  return loading ? (
-    <LoadingScreen />
-  ) : (
+  return (
     <Provider store={store}>
       <GestureHandlerRootView>
         <StatusBar barStyle="dark-content" backgroundColor={Color.white} />
